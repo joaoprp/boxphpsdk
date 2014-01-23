e6a33d69ed26ca2f84f0fa852396dac87667d001
Joao Paulo Rabelo <joaoprabelo@gmail.com>
2014-01-23 08:25:11 -0200

    first add for class box and class curl added

---
+++ curl.php
@@ -0,0 +1,55 @@
+<?php
+
+class Curl {
+	
+	/* set of curl methods */
+
+	public function get($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+	
+	public function post($url, $params) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+	
+	public function put($url, array $params = array()) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+	
+	public function delete($url, $params = '') {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+
+}
+
+?>
